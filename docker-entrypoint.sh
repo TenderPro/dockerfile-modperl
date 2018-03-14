5a5557abdddf37f7f4aae8574dd9600259ccb9ab
max.danilin <elatica7@gmail.com>
2018-03-14 17:43:26 +0300

    v0.0.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,13 +12,14 @@ if [[ "$APPUSER" ]]; then
   grep -qe "^$APPUSER:" /etc/passwd || useradd -m -r -s /bin/bash -Gwww-data -gusers -gsudo $APPUSER
 fi
 
+
 # Change user id to FLAG owner's uid
-FLAG_UID=$(stat -c "%u" $FLAG)
-if [[ "$FLAG_UID" ]] && [[ $FLAG_UID != $(id -u $APPUSER) ]]; then
-  echo "Set uid $FLAG_UID for user $APPUSER"
-  usermod -u $FLAG_UID $APPUSER
-  chown -R $APPUSER /home/op
-fi
+#FLAG_UID=$(stat -c "%u" $FLAG)
+#if [[ "$FLAG_UID" ]] && [[ $FLAG_UID != $(id -u $APPUSER) ]]; then
+#  echo "Set uid $FLAG_UID for user $APPUSER"
+#  usermod -u $FLAG_UID $APPUSER
+#  chown -R $APPUSER /home/op
+#fi
 
 export PATH=/usr/lib/node_modules/.bin:$PATH
 export NODE_PATH=/usr/lib/node_modules
@@ -26,5 +27,9 @@ export NODE_PATH=/usr/lib/node_modules
 # Add link to global modules
 [ -L $root/web_loaders ] || ln -s /usr/lib/node_modules $root/web_loaders
 
-echo "Run main shell.."
-gosu $APPUSER $@
+#echo "Run main shell.."
+# set -x \
+# &&gosu $APPUSER $@
+
+exec "/usr/bin/supervisord"
+#exec "$@"
